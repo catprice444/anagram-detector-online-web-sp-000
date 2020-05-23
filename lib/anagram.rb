567581442773589c8eba70587cb95b96d81725d1
Catherine Price <catprice444@gmail.com>
2020-05-23 18:50:19 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+
+# Your class, `Anagram` should take a word on initialization, and instances should
+# respond to a `match` method that takes an array of possible anagrams. It should
+# return all matches in an array. If no matches exist, it should return an empty
+# array.
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+def self.match(array)
+  array
+end
+
+end

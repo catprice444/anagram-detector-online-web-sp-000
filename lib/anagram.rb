37d96fea3be81dadec55bde295763279e6757906
Catherine Price <catprice444@gmail.com>
2020-05-23 18:59:43 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,15 +13,19 @@ class Anagram
 
   # take in an array
   # returns an empty array if you matches
-  # iterate over the array of words using the ".math" method
+  # iterate over the array of words using the ".match" method
   # compare the new array of words to the :word method
   # have to determine if they are composed of the same letters
   # use the ".split" method to seperate out the words in the array
   # use the "==" to compare the words/ arrays
   # can use the ".sort" method to compare the words/arrays
 
+  # what I have to do is take in an array of words and compare them (.match)
+  # then I have to compare the two words but when I compare then I have to (.split) and (.sort)
+
 def match(array)
-  array(split)
+  array.match do |word|
+    (@words.split(",").sort) == (word.split(",").sort)
 end
 
 end
